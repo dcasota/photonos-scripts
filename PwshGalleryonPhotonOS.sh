e540b79ce58806044f812b7a900d60088df36157
dcasota <dcasota@gmail.com>
2019-10-02 19:16:50 +0200

    Add files via upload

--- PwshGalleryonPhotonOS.sh
+++ PwshGalleryonPhotonOS.sh
@@ -10,8 +10,8 @@ yum -y install gcc automake autoconf libtool make bzip2 >> /tmp/myScript.txt
 # wget https://download.mono-project.com/sources/mono/mono-5.18.1.28.tar.bz2 >> /tmp/myScript.txt
 wget https://download.mono-project.com/sources/mono/mono-5.18.0.225.tar.bz2 >> /tmp/myScript.txt
 mkdir ~/mono >> /tmp/myScript.txt
-tar xfvj mono-5.18.1.28.tar.bz2 -C ~/mono >> /tmp/myScript.txt
-cd ~/mono/mono-5.18.1.28 >> /tmp/myScript.txt
+tar xfvj mono-5.18.0.225.tar.bz2 -C ~/mono >> /tmp/myScript.txt
+cd ~/mono/mono-5.18.0.225 >> /tmp/myScript.txt
 ./configure --prefix=/usr/local >> /tmp/myScript.txt
 make >> /tmp/myScript.txt
 make install >> /tmp/myScript.txt
