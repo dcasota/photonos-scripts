48ed3c0beb4f6bd299e4acb8eef6af8dea4bae61
dcasota <14890243+dcasota@users.noreply.github.com>
2019-10-15 14:20:35 +0200

    Add files via upload

---
+++ Pwsh7OnPhotonOS.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+# Deploy Powershell7 on VMware Photon OS
+#
+# This script deploys Powershell7 (Preview4) on VMware Photon OS.
+# The reference installation procedure for Pwsh7 on Linux was published on
+# https://docs.microsoft.com/en-us/powershell/scripting/install/installing-powershell-core-on-linux?view=powershell-7
+#
+#
+# Prerequisites:
+#    VMware Photon OS 3.0
+#
+# 
+
+tdnf -y update
+
+sudo apt-get update
+# install the requirements
+tdnf -y install -y \
+        less \
+        locales \
+        ca-certificates \
+        libicu63 \
+        libssl1.1 \
+        libc6 \
+        libgcc1 \
+        libgssapi-krb5-2 \
+        liblttng-ust0 \
+        libstdc++6 \
+        zlib1g \
+        curl
+
+# Download the powershell '.tar.gz' archive
+curl -L https://github.com/PowerShell/PowerShell/releases/download/v7.0.0-preview.4/powershell-7.0.0-preview.4-linux-x64.tar.gz -o /tmp/powershell.tar.gz
+
+# Create the target folder where powershell will be placed
+sudo mkdir -p /opt/microsoft/powershell/7-preview
+
+# Expand powershell to the target folder
+sudo tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/7-preview
+
+# Set execute permissions
+sudo chmod +x /opt/microsoft/powershell/7-preview/pwsh
+
+# Create the symbolic link that points to pwsh
+sudo ln -s /opt/microsoft/powershell/7-preview/pwsh /usr/bin/pwsh-preview
+
+# Start PowerShell
+pwsh-preview
+
+
+
