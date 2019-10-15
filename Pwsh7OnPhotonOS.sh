d5031e0a748394353d24da2c9e9de000d53d60b8
dcasota <14890243+dcasota@users.noreply.github.com>
2019-10-15 14:34:35 +0200

    Add files via upload

--- Pwsh7OnPhotonOS.sh
+++ Pwsh7OnPhotonOS.sh
@@ -19,8 +19,9 @@ tdnf -y update
 # install the requirements
 tdnf install -y \
         less \
-        # locales \
         ca-certificates \
+        curl
+        # locales \
         # libicu63 \
         # libssl1.1 \
         # libc6 \
@@ -28,8 +29,8 @@ tdnf install -y \
         # libgssapi-krb5-2 \
         # liblttng-ust0 \
         # libstdc++6 \
-        # zlib1g \
-        curl
+        # zlib1g
+
 
 # Download the powershell '.tar.gz' archive
 curl -L https://github.com/PowerShell/PowerShell/releases/download/v7.0.0-preview.4/powershell-7.0.0-preview.4-linux-x64.tar.gz -o /tmp/powershell.tar.gz
