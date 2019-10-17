786f727d9b3444b6c58eb87c0213f4ece0d50dd5
dcasota <14890243+dcasota@users.noreply.github.com>
2019-10-17 21:30:19 +0200

    Add files via upload

--- PwshGalleryonPhotonOS.sh
+++ PwshGalleryonPhotonOS.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# This study script makes the Microsoft PowerShellGallery available on Photon OS by using Mono with Nuget.
+# This study script makes Microsoft Powershell Core and the PowerShellGallery available on Photon OS by using Mono with Nuget.
 # 
 # Installing PowerShell Core on Photon OS does not built-in register PSGallery or nuget.org as source provider.
 # One way to accomplish it is using a tool from the Microsoft open source Nuget ecosystem.
@@ -45,34 +45,52 @@ yum -y install bison gettext glib2 freetype fontconfig libpng libpng-devel
 yum -y install gcc automake autoconf libtool make bzip2
 
 echo "$(date) + Downloading and installing Mono ..."
-sudo mkdir ~/mono
+mkdir ~/mono
 cd ~/mono
 curl -o $monofilename $monourl
-sudo tar -xvf $monofilename -C ~/mono
+tar -xvf $monofilename -C ~/mono
 cd ~/mono/$monodir
 # ./configure --prefix=/usr/local --enable-small-config=yes --enable-minimal=aot,profiler
-sudo ./configure --prefix=/usr/local
-sudo make -j4
-sudo make install
+./configure --prefix=/usr/local
+make -j4
+make install
 
 echo "$(date) + Downloading and Configuring PwshGallery Source by Mono with Nuget ..."
 curl -o /usr/local/bin/nuget.exe https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
-sudo mono /usr/local/bin/nuget.exe sources Add -Name PSGallery -Source "https://www.powershellgallery.com/api/v2"
-sudo mono /usr/local/bin/nuget.exe sources Add -Name nuget.org -Source "https://www.nuget.org/api/v2"
+mono /usr/local/bin/nuget.exe sources Add -Name PSGallery -Source "https://www.powershellgallery.com/api/v2"
+mono /usr/local/bin/nuget.exe sources Add -Name nuget.org -Source "https://www.nuget.org/api/v2"
 
 echo "$(date) + Installing Powershell ..."
-tdnf -y install powershell
+# This part is in reference from https://github.com/vmware/powerclicore/blob/master/Dockerfile
+# Install PowerShell and unzip on Photon
+tdnf install -y powershell unzip
 
-echo "$(date) + Installing Powershell modules packagemanagement and powershellget ..."
-pwsh -c '$PSVersionTable'
-pwsh -c 'install-module packagemanagement -Scope AllUsers -force'
-pwsh -c 'install-module powershellget -Scope AllUsers -force'
-# pwsh -c 'Set-PSRepository -Name PSGallery -InstallationPolicy Trusted'
+# Set working directory so stuff doesn't end up in /
+cd /root
+
+# Install PackageManagement and PowerShellGet
+# This is temporary until it is included in the PowerShell Core package for Photon
+curl -O -J -L https://www.powershellgallery.com/api/v2/package/PackageManagement
+unzip PackageManagement -d /usr/lib/powershell/Modules/PackageManagement
+rm -f PackageManagement
+
+curl -O -J -L https://www.powershellgallery.com/api/v2/package/PowerShellGet
+unzip PowerShellGet -d /usr/lib/powershell/Modules/PowerShellGet
+rm -f PowerShellGet
+
+# Workaround for https://github.com/vmware/photon/issues/752
+mkdir -p /usr/lib/powershell/ref/
+ln -s /usr/lib/powershell/*.dll /usr/lib/powershell/ref/
+
+pwsh -c 'Set-PSRepository -Name PSGallery -InstallationPolicy Trusted'
 
 echo "$(date) + Cleanup ..."
 cd ~/
-# Remove unnecessary installed prerequisites of Mono
+# Final clean up
+tdnf erase -y unzip
+tdnf clean all
 rm ~/mono/$monofilename
+# Remove unnecessary installed prerequisites of Mono
 
 echo "$(date) + Installing Microsoft PowerShellGallery on Photon OS by using Mono with Nuget finished."
 
