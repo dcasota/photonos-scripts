e354c19b132db5072e9f71986d8b96b63336fde6
dcasota <14890243+dcasota@users.noreply.github.com>
2019-10-30 17:15:07 +0100

    Add files via upload

--- Pwsh6.2.3OnPhotonOSCleanup.sh
+++ Pwsh6.2.3OnPhotonOSCleanup.sh
@@ -3,10 +3,10 @@
 rm /usr/bin/Pwsh*
 rm -r /opt/microsoft
 rm -r /tmp/Microsoft.PackageManagement
-rm -r /tmp/*.zip
 rm -r /tmp/photonos-scripts-master
 rm /tmp/Core*
-rm /tmp/tmp*
+rm /tmp/tmp*.ps1
+rm /tmp/*.zip
 rm -r /root/.cache/powershell
 rm -r /root/.local/share/powershell
 rm -r /usr/local/share/powershell
