279adc560ae9b4e188930db5769be8c0dff84caf
dcasota <14890243+dcasota@users.noreply.github.com>
2019-10-30 21:08:42 +0100

    Add files via upload

--- Pwsh6.2.3OnPhotonOS.sh
+++ Pwsh6.2.3OnPhotonOS.sh
@@ -5,38 +5,54 @@
 #
 #
 # History
-# 0.1  28.10.2019   dcasota  UNFINISHED! WORK IN PROGRESS!
+# 0.1  28.10.2019   dcasota  Initial release
 #
 # Prerequisites:
 #    - VMware Photon OS 3.0
 #    - No Powershell release installed
 #
 #
+# Description:
 # 'tndf install -y powershell' latest release is 6.1.0 and outdated (October 2019).
-# Powershell Core built-in installs the modules PackageManagement and PowerShellGet. Built-in means that automatic update functionality for its modules is included too.
+#    Powershell Core built-in installs the modules PackageManagement and PowerShellGet. Built-in means that automatic update functionality for its modules is included too.
 #
 # With Powershell Core 6.1.0 release this built-in automatic update functionality is broken. Note that the cmdlets find-module and install-module produces errors.
-# There are a few workaround possibilities. Keep in mind, applying a workaround means that with specific modules not installed by using install-module, it cannot be updated.
-# If this is not supported in your environment, use 'tdnf install -y powershell'. Sooner or later newer published releases are available.
+#    There are a few workaround possibilities. Keep in mind, applying a workaround means that with specific modules not installed by using install-module, it cannot be updated.
+#    If this is not supported in your environment, use 'tdnf install -y powershell'. Sooner or later newer published releases are available.
 # 
-# This script provides a workaround solution. It downloads and installs Powershell Core 6.2.3 release and installs the module PackageManagement 1.1.7.0.
+# This script provides a workaround solution. It downloads and installs Powershell Core 6.2.3 release, installs the module PackageManagement 1.1.7.0 and saves
+#    necessary prerequisites in profile /opt/microsoft/powershell/6.2.3/profile.ps1.
 #
-# Powershell is installed in /opt/microsoft/powershell/6.2.3/ with a symbolic link "Pwsh6.2.3" that points to /opt/microsoft/powershell/6.2.3/pwsh.
+#    Powershell is installed in /opt/microsoft/powershell/6.2.3/ with a symbolic link "Pwsh6.2.3" that points to /opt/microsoft/powershell/6.2.3/pwsh.
 #
-# The built-in module PowerShellGet version 2.1.3 in Powershell Core 6.2.3 has a RequiredModules specification of PackageManagement 1.1.7.0.
-# Using an embedded powershell script it installs PackageManagement 1.1.7.0.
-# The dynamically created powershell script provides three helper functions used as cmdlets workaround:
-# - workaround.Find-ModuleAllVersions
-# - workaround.Save-Module
-# - workaround.Install-NugetPkgOnLinux
-# The powershell script allows to specify Package Management and PowerShellGet version. See '\$PackageManagementVersion="1.1.7.0"'.
+#    The built-in module PowerShellGet version 2.1.3 in Powershell Core 6.2.3 has a RequiredModules specification of PackageManagement 1.1.7.0.
+#    The embedded powershell script installs PackageManagement 1.1.7.0. It provides three helper functions used as cmdlets workaround:
+#    - workaround.Find-ModuleAllVersions
+#    - workaround.Save-Module
+#    - workaround.Install-NugetPkgOnLinux
+#    The powershell script allows to specify Package Management and PowerShellGet version. See '\$PackageManagementVersion="1.1.7.0"'.
 #
-# After the installation, the functionality of find-module, install-module, get-psrepository, etc. is back.
+#    Two workarounds are necessary to be saved in profile /opt/microsoft/powershell/$ReleaseDir/profile.ps1.
+#       Each time Pwsh$ReleaseDir is started the saved profile with the workarounds is loaded.
+#       #https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_profiles?view=powershell-5.1&redirectedfrom=MSDN
+#       Show variables of $PROFILE:
+#       $PROFILE | Get-Member -Type NoteProperty
 #
-# Limitations:
-# - side effects with already installed powershell releases not tested
-# - proxy functionality not tested
-# - various constellations with security protocol policies or with cert policies not tested
+#       Workaround #1
+#       https://github.com/PowerShell/PowerShellGet/issues/447#issuecomment-476968923
+#       Change to TLS1.2
+#       [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+#
+#       Workaround #2
+#       https://github.com/PowerShell/PowerShell/issues/9495#issuecomment-515592672
+#       $env:DOTNET_SYSTEM_NET_HTTP_USESOCKETSHTTPHANDLER=0
+#
+#    After the installation, the functionality of find-module, install-module, get-psrepository, etc. is back.
+#
+# Limitations / not tested:
+# - side effects with already installed powershell releases
+# - proxy functionality
+# - various constellations with security protocol policies or with cert policies
 #
 
 # install the requirements
@@ -56,21 +72,8 @@ DownloadURL="https://github.com/PowerShell/PowerShell/releases/download/v6.2.3/p
 ReleaseDir="6.2.3"
 PwshLink=Pwsh$ReleaseDir
 
-ToInstall=true
-if (`command -v pwsh`); then
-	OUTPUT=`pwsh -c '$PSVersiontable'`
-	if (!(echo $OUTPUT | grep -q "$ReleaseDir")); then
-		ToInstall=false
-	fi
-fi
-if [ -d /opt/microsoft/powershell/$ReleaseDir/pwsh ]; then
-	OUTPUT=`/opt/microsoft/powershell/$ReleaseDir/pwsh -c '$PSVersiontable'`
-	if (!(echo $OUTPUT | grep -q "$ReleaseDir")); then
-		ToInstall=false
-	fi
-fi
-
-if ("$ToInstall" = "true"); then
+# Install powershell 6.2.3
+if (![ -d /opt/microsoft/powershell/$ReleaseDir/pwsh ]); then
 	# Download the powershell '.tar.gz' archive
 	curl -L $DownloadURL -o /tmp/powershell.tar.gz
 	# Create the target folder where powershell will be placed
@@ -83,38 +86,11 @@ if ("$ToInstall" = "true"); then
 	ln -s /opt/microsoft/powershell/$ReleaseDir/pwsh /usr/bin/$PwshLink
 	# delete downloaded file
 	rm /tmp/powershell.tar.gz
+fi
 
-
-	RunEmbeddedScript=true
-	OUTPUT=`/opt/microsoft/powershell/$ReleaseDir/pwsh -c 'get-psrepository'`
-	if (echo $OUTPUT | grep -q "PSGallery"); then
-		echo "$ReleaseDir: PSGallery is registered."
-		# Check: PSGallery is browseable using "find-module".
-		OUTPUT=`/opt/microsoft/powershell/$ReleaseDir/pwsh -c "find-module VMware.PowerCLI"`
-		if (echo $OUTPUT | grep -q "PSGallery"); then
-			echo "$ReleaseDir: PSGallery is browseable."
-			echo "$ReleaseDir: All provisioning tests successfully processed."
-			RunEmbeddedScript=false
-		else
-			echo "ERROR: PSGallery not detected as browseable."
-		fi		
-	else
-		echo "PSGallery not detected as registered."
-	fi
-
-	if ("$RunEmbeddedScript" = "true"); then
 	
 # Prepare helper functions content
 IFS='' read -r -d '' PSContent1 << "EOF1"
-function LogfileAppend($text)
-{
-	$TimeStamp = (get-date).ToString('dd.MM.yyyy HH:mm:ss.fff')
-	Write-Host $TimeStamp  $text
-}
-EOF1
-
-
-IFS='' read -r -d '' PSContent2 << "EOF2"
 function workaround.Find-ModuleAllVersions
 {
 	# https://stackoverflow.com/questions/37486587/powershell-v5-how-to-install-modules-to-a-computer-having-no-internet-connecti
@@ -163,10 +139,10 @@ function workaround.Find-ModuleAllVersions
 		}
 	}
 }
-EOF2
+EOF1
 
 
-IFS='' read -r -d '' PSContent3 << "EOF3"
+IFS='' read -r -d '' PSContent2 << "EOF2"
 function workaround.Save-Module
 {
 	param (
@@ -199,9 +175,9 @@ function workaround.Save-Module
 	$rc = Get-Item $Path
 	return $rc
 }
-EOF3
+EOF2
 
-IFS='' read -r -d '' PSContent4 << "EOF4"
+IFS='' read -r -d '' PSContent3 << "EOF3"
 function workaround.Install-NugetPkgOnLinux
 {
 	param (
@@ -233,22 +209,21 @@ function workaround.Install-NugetPkgOnLinux
             if ($Versionstring -eq "") {$Versionstring = ($PackageFileName.split("."))[$i]}
             else { $VersionString = $VersionString + "." + ($PackageFileName.split("."))[$i]}
         }
-		LogfileAppend("VersionString = $VersionString")
 		
 		if ($VersionString -imatch $PackageVersion)
 		{
-			LogfileAppend("Unzipping $Sourcefile to $destinationpath ...")	
+			# Unzipping $Sourcefile to $destinationpath	
 			unzip -o $Sourcefile -d $destinationpath		
 			chmod -R 755 $(find $destinationpath -type d)
 			chmod -R 644 $(find $destinationpath -type f)
 			
-			LogfileAppend("Removing $sourcefile ...")
+			# Removing $sourcefile
 			remove-item -path ($Sourcefile) -force -recurse -confirm:$false
 			
+			# Parse and import all .psd1 files
 			get-childitem -path $destinationpath -recurse -filter *.psd1| ? {
 				$TmpFile = $destinationpath + $PathDelimiter + $_.Name
-				try {
-					LogfileAppend("Import $TmpFile ...")			
+				try {		
 					import-module -name $TmpFile -Scope Global -Verbose -force -erroraction silentlycontinue
 				} catch {}
 			}
@@ -268,9 +243,9 @@ function workaround.Install-NugetPkgOnLinux
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 # Verify again current TLS support of powershell
 [Net.ServicePointManager]::SecurityProtocol
-EOF4
+EOF3
 
-IFS='' read -r -d '' PSContent5 << "EOF5"
+IFS='' read -r -d '' PSContent4 << "EOF4"
 try
 {
 	$InstallPackageManagement = $false
@@ -285,11 +260,9 @@ try
 	}
 	if ($InstallPackagemanagement -eq $true)
 	{
-		LogfileAppend("Installing Packagemanagement release $PackageManagementVersion ...")
-		$rc = workaround.Find-ModuleAllVersions -name packagemanagement -version "$PackageManagementVersion" | workaround.Save-Module -Path "$PSHome/Modules"
-		LogfileAppend("Installing Packagemanagement release $PackageManagementVersion : return code $rc")				
+		# Install Packagemanagement release $PackageManagementVersion
+		$rc = workaround.Find-ModuleAllVersions -name packagemanagement -version "$PackageManagementVersion" | workaround.Save-Module -Path "$PSHome/Modules"			
 		$rc = workaround.Install-NugetPkgOnLinux "PackageManagement" "$PackageManagementVersion" $rc.name "$PSHome/Modules" "$PSHome/Modules"
-		LogfileAppend("Installing Packagemanagement release $PackageManagementVersion done : return code $rc")
 	}
 
 	$InstallPowerShellGet = $false
@@ -304,71 +277,55 @@ try
 	}
 	if ($InstallPowerShellGet -eq $true)
 	{
-		LogfileAppend("Installing PowerShellGet release $PowerShellGetVersion ...")
-		$rc = workaround.Find-ModuleAllVersions -name PowerShellGet -version "$PowerShellGetVersion" | workaround.Save-Module -Path "$PSHome/Modules"
-		LogfileAppend("Installing PowerShellGet release $PowerShellGetVersion : return code $rc")				
+		# InstallPowerShellGet release $PowerShellGetVersion
+		$rc = workaround.Find-ModuleAllVersions -name PowerShellGet -version "$PowerShellGetVersion" | workaround.Save-Module -Path "$PSHome/Modules"			
 		$rc = workaround.Install-NugetPkgOnLinux "PowerShellGet" "$PowerShellGetVersion" $rc.name "$PSHome/Modules" "$PSHome/Modules"
-		LogfileAppend("Installing PowerShellGet release $PowerShellGetVersion done : return code $rc")
 	}				
 }
 catch { }
 # if ((Get-PSRepository -name psgallery | %{ $_.InstallationPolicy -match "Untrusted" }) -eq $true) { set-psrepository -name PSGallery -InstallationPolicy Trusted }
-EOF5
+EOF4
 
 
-	
-# PowerShellGet release 2.1.3 has RequiredModules specification of PackageManagement 1.1.7.0. Use the helper functions to install modules.			
-cat <<EOF1170213 > /tmp/tmp1.ps1
-# Post-installation for PowerShell 6.2.3
+# PowerShellGet release 2.1.3 has RequiredModules specification of PackageManagement 1.1.7.0.
+# The dynamically created powershell script contains helper functions which install the specified releases of the modules.
+#
+# Check functionality of powershell 6.2.3
+OUTPUT=`/opt/microsoft/powershell/$ReleaseDir/pwsh -c "find-module VMware.PowerCLI"`
+if (!(echo $OUTPUT | grep -q "PSGallery")); then
+
+	tmpfile=/tmp/tmp1.ps1		
+	cat <<EOF1170213 > $tmpfile
+# 
 $PSContent1
 $PSContent2
 $PSContent3
-$PSContent4
 \$PackageManagementVersion="1.1.7.0"
 \$PowerShellGetVersion="2.1.3"
-$PSContent5
-get-psrepository
-find-module VMware.PowerCLI       
+$PSContent4     
 EOF1170213
-$PwshLink -c '/tmp/tmp1.ps1' -WorkingDirectory /tmp
-rm /tmp/tmp1.ps1
+	$PwshLink -c $tmpfile -WorkingDirectory /tmp
+	rm $tmpfile
 
-
-# Save modules functionality workarounds in profile. Each time Pwsh$ReleaseDir is started the profile is loaded as well.
-# #https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_profiles?view=powershell-5.1&redirectedfrom=MSDN
-#
-# Workaround1:
-# https://github.com/PowerShell/PowerShellGet/issues/447#issuecomment-476968923
-# Change to TLS1.2
-# [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-#
-# Workaround2:
-# https://github.com/PowerShell/PowerShell/issues/9495#issuecomment-515592672
-# $env:DOTNET_SYSTEM_NET_HTTP_USESOCKETSHTTPHANDLER=0  
-#
-cat <<EOFProfile > /opt/microsoft/powershell/6.2.3/profile.ps1
+	# Two workarounds are necessary to be saved in profile /opt/microsoft/powershell/$ReleaseDir/profile.ps1.
+	# Each time Pwsh$ReleaseDir is started the saved profile with the workarounds is loaded.
+	# #https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_profiles?view=powershell-5.1&redirectedfrom=MSDN
+	# Show variables of $PROFILE:
+	# $PROFILE | Get-Member -Type NoteProperty
+	#
+	# Workaround #1
+	# https://github.com/PowerShell/PowerShellGet/issues/447#issuecomment-476968923
+	# Change to TLS1.2
+	# [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+	#
+	# Workaround #2
+	# https://github.com/PowerShell/PowerShell/issues/9495#issuecomment-515592672
+	# $env:DOTNET_SYSTEM_NET_HTTP_USESOCKETSHTTPHANDLER=0  
+	#
+	cat <<EOFProfile > /opt/microsoft/powershell/$ReleaseDir/profile.ps1
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 \$env:DOTNET_SYSTEM_NET_HTTP_USESOCKETSHTTPHANDLER=0     
 EOFProfile
-
-
-OUTPUT=`/opt/microsoft/powershell/$ReleaseDir/pwsh -c 'get-psrepository'`
-if (echo $OUTPUT | grep -q "PSGallery"); then
-	echo "$ReleaseDir: PSGallery is registered."	
-	# Check: PSGallery is browseable using "find-module".
-	OUTPUT=`/opt/microsoft/powershell/$ReleaseDir/pwsh -c "find-module VMware.PowerCLI"`
-	if (echo $OUTPUT | grep -q "PSGallery"); then
-		echo "$ReleaseDir: PSGallery is browseable."
-		echo "$ReleaseDir: All provisioning tests successfully processed."
-	else
-		echo "ERROR: PSGallery not detected as browseable."
-	fi		
-else
-	echo "PSGallery not detected as registered."
-fi
-
-
-fi
 
 fi
 
