0c5fa24bd9aa3fbd2e92858cddb9577403cde226
dcasota <14890243+dcasota@users.noreply.github.com>
2021-04-17 18:11:29 +0200

    Add files via upload

--- photonos-package-report.ps1
+++ photonos-package-report.ps1
@@ -5,6 +5,7 @@
 #   Author:  Daniel Casota
 #   Version:
 #   0.1   06.03.2021   dcasota  First release
+#   0.2   17.04.2021   dcasota  dev added
 #
 #  .PREREQUISITES
 #    - Script actually tested only on MS Windows OS with Powershell PSVersion 5.1 or higher
@@ -50,15 +51,16 @@ function ParseDirectory
 
 # EDIT
 # path with all downloaded and unzipped branch directories of github.com/vmware/photon
-$sourcepath="C:\Users\myname\Downloads"
+$sourcepath="C:\Users\username\Downloads"
 # output file
-$outputfile="C:\Users\myname\Downloads\photonos-package-report.prn"
+$outputfile="C:\Users\username\Downloads\photonos-package-report.prn"
 
 #arrays
 $Packages1=ParseDirectory -SourcePath $sourcepath -PhotonDir photon-1.0
 $Packages2=ParseDirectory -SourcePath $sourcepath -PhotonDir photon-2.0
 $Packages3=ParseDirectory -SourcePath $sourcepath -PhotonDir photon-3.0
 $Packages4=ParseDirectory -SourcePath $sourcepath -PhotonDir photon-4.0
+$Packages5=ParseDirectory -SourcePath $sourcepath -PhotonDir photon-dev
 $PackagesMaster=ParseDirectory -SourcePath $sourcepath -PhotonDir photon-master
 
 # merge
@@ -67,8 +69,9 @@ $result = $Packages1,$Packages2,$Packages3,$Packages4,$PackagesMaster| %{$_}|Sel
 @{l='photon-2.0';e={if($_.Spec -in $Packages2.Spec) {$Packages2[$Packages2.Spec.IndexOf($_.Spec)].version}}},`
 @{l='photon-3.0';e={if($_.Spec -in $Packages3.Spec) {$Packages3[$Packages3.Spec.IndexOf($_.Spec)].version}}},`
 @{l='photon-4.0';e={if($_.Spec -in $Packages4.Spec) {$Packages4[$Packages4.Spec.IndexOf($_.Spec)].version}}},`
+@{l='photon-dev';e={if($_.Spec -in $Packages5.Spec) {$Packages5[$Packages5.Spec.IndexOf($_.Spec)].version}}},`
 @{l='photon-master';e={if($_.Spec -in $PackagesMaster.Spec) {$PackagesMaster[$PackagesMaster.Spec.IndexOf($_.Spec)].version}}} -Unique | Sort-object Spec
 
 # write output
-"Spec"+","+"photon-1.0"+","+"photon-2.0"+","+"photon-3.0"+","+"photon-4.0"+","+"photon-Master" |  out-file $outputfile
-$result | % { $_.Spec+","+$_."photon-1.0"+","+$_."photon-2.0"+","+$_."photon-3.0"+","+$_."photon-4.0"+","+$_."photon-Master"} |  out-file $outputfile -append
+"Spec"+","+"photon-1.0"+","+"photon-2.0"+","+"photon-3.0"+","+"photon-4.0"+","+"photon-dev"+","+"photon-master"|  out-file $outputfile
+$result | % { $_.Spec+","+$_."photon-1.0"+","+$_."photon-2.0"+","+$_."photon-3.0"+","+$_."photon-4.0"+","+$_."photon-dev"+","+$_."photon-master"} |  out-file $outputfile -append
